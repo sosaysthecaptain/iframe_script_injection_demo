2df1cc21a482eec0777c4bd0140bacb2f2618f51
Marc Auger <sosaysthecaptain@gmail.com>
2019-05-07 19:15:24 -0400

    iframe getting injected

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-let target_div = document.getElementById('put-iframe-here')
-debugger
-console.log(target_div)
+// Get the target div, empty it
+let target_div = $('#put-iframe-here')
+target_div.empty()
+
+// Construct the new thing we want to inject
+var simple_div = $(`
+    <div>this is stuff getting injected</div>
+`)
+
+simple_div.css('background-color', 'lightblue')
+simple_div.css('height', 200)
+simple_div.css('padding', 20)
+
+// Inject it
+// target_div.append(simple_div)
+
+// iFrame example
+var iframe_div = $(`
+<iframe src="http://google.com" 
+    frameborder="1" 
+    height="600" 
+    width="600">
+        alternative content for browsers which do not support iframe.
+    </iframe>
+`)
+
+target_div.append(iframe_div)
